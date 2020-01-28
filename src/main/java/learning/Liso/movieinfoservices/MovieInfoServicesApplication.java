e0aa5aecc01a061d396a82a480aa22c408527048
lisjana.allkja <lisjana.allkja@gmail.com>
2020-01-28 22:34:58 +0100

    Initial Commit

---
+++ src/main/java/learning/Liso/movieinfoservices/MovieInfoServicesApplication.java
@@ -0,0 +1,13 @@
+package learning.Liso.movieinfoservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServicesApplication.class, args);
+	}
+
+}
